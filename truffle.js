2c2f791278caf591997c2d0ed170f25e5ad484fc
Shane van Coller <svancoller@productiveedge.com>
2017-12-06 21:15:47 -0600

    Updated deployment scripts and mnemonics

--- truffle.js
+++ truffle.js
@@ -1,5 +1,7 @@
+var HDWalletProvider = require("truffle-hdwallet-provider");
 require('babel-register');
 require('babel-polyfill');
+require('dotenv').config();
 
 module.exports = {
     networks: {
@@ -7,6 +9,14 @@ module.exports = {
             host: "localhost",
             port: 8545,
             network_id: "*" // Match any network id
-        }
+        },
+        ropsten: {
+            provider: function() {
+                return new HDWalletProvider(process.env.ROPSTEN_MNEMONIC, "https://ropsten.infura.io/GC0NQf10QgUudzUe5B54")
+            },
+            gas: 4600000,
+            gasPrice: 21000000000,
+            network_id: "3"
+        },
     }
 };
