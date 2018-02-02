ed3ce9193ba905f6d266bb3f88204c22aa4bff8d
Sergey Ilin <ilinserg@gmail.com>
2018-02-02 14:37:26 -0600

    Notification if there are no cases in the queue

--- dapp/src/screens/Physician Profile/components/GetCase.js
+++ dapp/src/screens/Physician Profile/components/GetCase.js
@@ -1,17 +1,36 @@
 import React, { Component } from 'react';
 import { withRouter } from 'react-router-dom';
+import { Modal } from 'react-bootstrap';
+import Spinner from '../../../components/Spinner';
 import { getNextCaseFromQueue } from '../../../utils/web3-util';
 import './GetCase.css';
 
 class GetCase extends Component {
+    constructor(){
+        super()
+
+        this.state = {
+            showNoCasesModal: false,
+            actionInProgress: false
+        };
+    }
+    
     navigateToDiagnoseCaseScreen = async () => {
+        this.setState({actionInProgress: true});
         const nextCaseAddress = await getNextCaseFromQueue();
+        this.setState({actionInProgress: false});
         
         if(nextCaseAddress == null) {
-
+            this.setState({showNoCasesModal: true});
         } else {
             this.props.history.push('/diagnose-case/' + nextCaseAddress);
         }
+    }
+
+    handleCloseNoCaseModal = (event) => {
+        event.preventDefault();
+        
+        this.setState({showNoCasesModal: false});
     }
   
     render() {
@@ -29,6 +48,19 @@ class GetCase extends Component {
                         </div>
                     </div>
                 </div>
+                <Modal show={this.state.showNoCasesModal}>
+                    <Modal.Body>
+                        <div className="row">
+                            <div className="col text-center">
+                                <h4>There are no cases in the queue.</h4>
+                            </div>
+                        </div>
+                    </Modal.Body>
+                    <Modal.Footer>
+                        <button onClick={this.handleCloseNoCaseModal} type="button" className="btn btn-defult">OK</button>
+                    </Modal.Footer>
+                </Modal>
+                <Spinner loading={this.state.actionInProgress}/>
             </div>
     );
   }
