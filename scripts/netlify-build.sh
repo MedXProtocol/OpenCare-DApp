5d59867c4af8e5b5732d24cf6121656f4c16bcf2
Brendan Asselstine <mail.asselstine@gmail.com>
2018-05-10 11:08:50 -0700

    Updated netlify build script to fail on error

--- scripts/netlify-build.sh
+++ scripts/netlify-build.sh
@@ -1,5 +1,5 @@
 #! /bin/sh
 
-truffle compile
-truffle-migrate-off-chain --network ropsten
+truffle compile && \
+truffle-migrate-off-chain --network ropsten && \
 cd dapp && npm install && npm run build && cd ..
