318d35c84552de2c7ac72d649ad9e068dd27d04a
Brendan Asselstine <mail.asselstine@gmail.com>
2018-05-31 16:53:55 -0700

    Renamed some files

--- dapp/src/sagas/index.js
+++ dapp/src/sagas/index.js
@@ -1,7 +1,7 @@
 import { all, fork, takeEvery } from 'redux-saga/effects'
 import rootSagaGenesis, { takeOnceAndRun } from '@/saga-genesis/sagas'
-import addTopLevelContracts from './add-top-level-contracts'
-import addRegistryContracts from './add-registry-contracts'
+import addTopLevelContracts from './add-top-level-contracts-saga'
+import addRegistryContracts from './add-registry-contracts-saga'
 
 export default function* () {
   yield fork(takeOnceAndRun, 'WEB3_INITIALIZED', addTopLevelContracts)
