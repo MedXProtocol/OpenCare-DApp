364caf9b68379feb5b38a8a37005677c41150220
Brendan Asselstine <mail.asselstine@gmail.com>
2018-05-31 16:02:18 -0700

    Refactored Diagnosis to use Saga Genesis

--- dapp/src/components/Diagnosis.js
+++ dapp/src/components/Diagnosis.js
@@ -1,165 +1,182 @@
-import React, { Component } from 'react';
-import { Modal } from 'react-bootstrap';
-import { withRouter } from 'react-router-dom';
-import PropTypes from 'prop-types';
-import Spinner from '@/components/Spinner';
-import { getCaseStatus, getCaseDoctorADiagnosisLocationHash, acceptDiagnosis, challengeDiagnosis } from '@/utils/web3-util';
-import { downloadJson } from '@/utils/storage-util';
+import React, { Component } from 'react'
+import { Modal } from 'react-bootstrap'
+import { withRouter } from 'react-router-dom'
+import PropTypes from 'prop-types'
+import Spinner from '@/components/Spinner'
+import { downloadJson } from '@/utils/storage-util'
+import { withSaga, cacheCall, cacheCallValue, withSend } from '@/saga-genesis'
+import { connect } from 'react-redux'
+import { getFileHashFromBytes } from '@/utils/get-file-hash-from-bytes'
 
-class Diagnosis extends Component {
-    constructor(){
-        super()
-
-        this.state = {
-            diagnosis: {},
-            status: {},
-            hidden: true,
-            buttonsHidden: true,
-            submitInProgress: false,
-            showThankYouModal: false,
-            showChallengeModal: false
-        };
-    }
-
-    async componentDidMount() {
-        const status = await getCaseStatus(this.props.caseAddress);
-
-        this.setState({status: status});
-
-        if(status.code === 5) {
-            this.setState({buttonsHidden: false});
-        }
-
-        const diagnosisHash = await getCaseDoctorADiagnosisLocationHash(this.props.caseAddress);
-
-        if(diagnosisHash !== null && diagnosisHash !== "0x") {
-            const diagnosisJson = await downloadJson(diagnosisHash, this.props.caseKey);
-            const diagnosis = JSON.parse(diagnosisJson);
-            this.setState({
-                diagnosis: diagnosis,
-                hidden: false
-            });
-        }
-    }
-
-    handleAcceptDiagnosis = async () => {
-        this.setState({submitInProgress: true});
-
-        acceptDiagnosis(this.props.caseAddress, (error, result) => {
-            if(error !== null) {
-                //this.onError(error);
-            } else {
-                this.onAcceptSuccess();
-            }
-        });
-    }
-
-    handleChallengeDiagnosis = async () => {
-        this.setState({submitInProgress: true});
-
-        challengeDiagnosis(this.props.caseAddress, (error, result) => {
-            if(error !== null) {
-                //this.onError(error);
-            } else {
-                this.onChallengeSuccess();
-            }
-        });
-    }
-
-    handleCloseThankYouModal = (event) => {
-        event.preventDefault();
-
-        this.setState({showThankYouModal: false});
-
-        this.props.history.push('/patient-profile');
-    }
-
-    handleCloseChallengeModal = (event) => {
-        event.preventDefault();
-
-        this.setState({showChallengeModal: false});
-
-        this.props.history.push('/patient-profile');
-    }
-
-    onAcceptSuccess = () => {
-        this.setState({
-            submitInProgress: false,
-            showThankYouModal: true
-        });
-    }
-
-    onChallengeSuccess = () => {
-        this.setState({
-            submitInProgress: false,
-            showChallengeModal: true
-        });
-    }
-
-    render() {
-        return ( this.state.hidden ?
-            <div /> :
-            <div className="card">
-                <div className="card-header">
-                    <h2 className="card-title">Diagnosis</h2>
-                </div>
-                <div className="card-content">
-                    <div className="row">
-
-                        <div className="col-xs-12">
-                            <label>Diagnosis</label>
-                            <p>{this.state.diagnosis.diagnosis}</p>
-                        </div>
-                        <div className="col-lg-6 col-md-12 top10">
-                            <label>Recommendation</label>
-                            <p>{this.state.diagnosis.recommendation}</p>
-                        </div>
-                    </div>
-                </div>
-
-                {
-                    this.state.buttonsHidden ? null :
-                    <div className="card-footer">
-                        <hr/>
-                        <div className="row">
-                            <div className="col-xs-12 text-center" >
-                                <button onClick={this.handleAcceptDiagnosis} type="button" className="btn btn-success btn-fill">Accept</button>
-                                &nbsp;
-                                <button onClick={this.handleChallengeDiagnosis} type="button" className="btn btn-danger btn-fill">Get Second Opinion</button>
-                            </div>
-                        </div>
-                    </div>
-                }
-
-                <Modal show={this.state.showThankYouModal}>
-                    <Modal.Body>
-                        <div className="row">
-                            <div className="col text-center">
-                                <h4>Thank you for using MedCredits!</h4>
-                            </div>
-                        </div>
-                    </Modal.Body>
-                    <Modal.Footer>
-                        <button onClick={this.handleCloseThankYouModal} type="button" className="btn btn-defult">OK</button>
-                    </Modal.Footer>
-                </Modal>
-                <Modal show={this.state.showChallengeModal}>
-                    <Modal.Body>
-                        <div className="row">
-                            <div className="col text-center">
-                                <h4>Another doctor will now review your case.</h4>
-                            </div>
-                        </div>
-                    </Modal.Body>
-                    <Modal.Footer>
-                        <button onClick={this.handleCloseChallengeModal} type="button" className="btn btn-defult">OK</button>
-                    </Modal.Footer>
-                </Modal>
-                <Spinner loading={this.state.submitInProgress}/>
-            </div>
-        );
-    }
+function mapStateToProps(state, { caseAddress, caseKey }) {
+  const status = cacheCallValue(state, caseAddress, 'status')
+  const diagnosisALocationHash = getFileHashFromBytes(cacheCallValue(state, caseAddress, 'diagnosisALocationHash'))
+  const transactions = state.sagaGenesis.transactions
+  return {
+    status,
+    diagnosisALocationHash,
+    transactions
+  }
 }
+
+function* saga({ caseAddress }) {
+  yield cacheCall(caseAddress, 'status')
+  yield cacheCall(caseAddress, 'diagnosisALocationHash')
+}
+
+const Diagnosis = connect(mapStateToProps)(withSaga(saga, { propTriggers: ['caseAddress'] })(withSend(class _Diagnosis extends Component {
+  constructor(){
+    super()
+
+    this.state = {
+      diagnosis: {},
+      status: {},
+      hidden: true,
+      buttonsHidden: true,
+      submitInProgress: false,
+      showThankYouModal: false,
+      showChallengeModal: false
+    }
+  }
+
+  async componentDidMount() {
+      const status = parseInt(this.props.status)
+
+      this.setState({status: status})
+
+      if(status === 5) {
+          this.setState({buttonsHidden: false})
+      }
+
+      const diagnosisHash = this.props.diagnosisALocationHash
+
+      if(diagnosisHash !== null && diagnosisHash !== "0x") {
+          const diagnosisJson = await downloadJson(diagnosisHash, this.props.caseKey);
+          const diagnosis = JSON.parse(diagnosisJson);
+          this.setState({
+              diagnosis: diagnosis,
+              hidden: false
+          });
+      }
+  }
+
+  componentWillReceiveProps (props) {
+    if (this.state.acceptTransactionId && props.transactions[this.state.acceptTransactionId].complete) {
+      this.onAcceptSuccess()
+    }
+    if (this.state.challengeTransactionId && props.transactions[this.state.challengeTransactionId].complete) {
+      this.onChallengeSuccess()
+    }
+  }
+
+  handleAcceptDiagnosis = () => {
+    const acceptTransactionId = this.props.send(this.props.caseAddress, 'acceptDiagnosis')()
+    this.setState({
+      submitInProgress: true,
+      acceptTransactionId
+    });
+  }
+
+  handleChallengeDiagnosis = () => {
+    const challengeTransactionId = this.props.send(this.props.caseAddress, 'challengeDiagnosis')()
+    this.setState({
+      submitInProgress: true,
+      challengeTransactionId
+    });
+  }
+
+  handleCloseThankYouModal = (event) => {
+      event.preventDefault();
+      this.setState({showThankYouModal: false});
+      this.props.history.push('/patient-profile');
+  }
+
+  handleCloseChallengeModal = (event) => {
+      event.preventDefault();
+      this.setState({showChallengeModal: false});
+      this.props.history.push('/patient-profile');
+  }
+
+  onAcceptSuccess = () => {
+    this.setState({
+      acceptTransactionId: null,
+      submitInProgress: false,
+      showThankYouModal: true
+    });
+  }
+
+  onChallengeSuccess = () => {
+    this.setState({
+      challengeTransactionId: null,
+      submitInProgress: false,
+      showChallengeModal: true
+    });
+  }
+
+  render() {
+      return ( this.state.hidden ?
+          <div /> :
+          <div className="card">
+              <div className="card-header">
+                  <h2 className="card-title">Diagnosis</h2>
+              </div>
+              <div className="card-content">
+                  <div className="row">
+
+                      <div className="col-xs-12">
+                          <label>Diagnosis</label>
+                          <p>{this.state.diagnosis.diagnosis}</p>
+                      </div>
+                      <div className="col-lg-6 col-md-12 top10">
+                          <label>Recommendation</label>
+                          <p>{this.state.diagnosis.recommendation}</p>
+                      </div>
+                  </div>
+              </div>
+
+              {
+                  this.state.buttonsHidden ? null :
+                  <div className="card-footer">
+                      <hr/>
+                      <div className="row">
+                          <div className="col-xs-12 text-center" >
+                              <button onClick={this.handleAcceptDiagnosis} type="button" className="btn btn-success btn-fill">Accept</button>
+                              &nbsp;
+                              <button onClick={this.handleChallengeDiagnosis} type="button" className="btn btn-danger btn-fill">Get Second Opinion</button>
+                          </div>
+                      </div>
+                  </div>
+              }
+
+              <Modal show={this.state.showThankYouModal}>
+                  <Modal.Body>
+                      <div className="row">
+                          <div className="col text-center">
+                              <h4>Thank you for using MedCredits!</h4>
+                          </div>
+                      </div>
+                  </Modal.Body>
+                  <Modal.Footer>
+                      <button onClick={this.handleCloseThankYouModal} type="button" className="btn btn-defult">OK</button>
+                  </Modal.Footer>
+              </Modal>
+              <Modal show={this.state.showChallengeModal}>
+                  <Modal.Body>
+                      <div className="row">
+                          <div className="col text-center">
+                              <h4>Another doctor will now review your case.</h4>
+                          </div>
+                      </div>
+                  </Modal.Body>
+                  <Modal.Footer>
+                      <button onClick={this.handleCloseChallengeModal} type="button" className="btn btn-defult">OK</button>
+                  </Modal.Footer>
+              </Modal>
+              <Spinner loading={this.state.submitInProgress}/>
+          </div>
+      );
+  }
+})))
 
 Diagnosis.propTypes = {
     caseAddress: PropTypes.string,
--- dapp/src/saga-genesis/transaction/transaction-reducer.js
+++ dapp/src/saga-genesis/transaction/transaction-reducer.js
@@ -1,8 +1,6 @@
 export default function (state, {type, transactionId, call, error, receipt}) {
   if (typeof state === 'undefined') {
-    state = {
-      transactions: {}
-    }
+    state = {}
   }
 
   switch (type) {
