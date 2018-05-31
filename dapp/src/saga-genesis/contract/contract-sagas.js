e6053f09610b42023b8ed5c47cd6bfa1a5bd1bcc
Brendan Asselstine <mail.asselstine@gmail.com>
2018-05-30 19:15:31 -0700

    Add contract saga only adds contract if it doesnt exists

--- dapp/src/saga-genesis/contract/contract-sagas.js
+++ dapp/src/saga-genesis/contract/contract-sagas.js
@@ -2,10 +2,16 @@ import {
   put,
   select
 } from 'redux-saga/effects'
+import {
+  contractKeyByAddress
+} from '../state-finders'
 
 export function* addContract({address, name, contractKey, networkId}) {
   if (!networkId) {
     networkId = yield select((state) => state.sagaGenesis.network.networkId)
   }
-  yield put({type: "ADD_CONTRACT", address, name, contractKey, networkId})
+  let existingContractKey = yield select(contractKeyByAddress, address)
+  if (existingContractKey !== contractKey) {
+    yield put({type: "ADD_CONTRACT", address, name, contractKey, networkId})
+  }
 }
