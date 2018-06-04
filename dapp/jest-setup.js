568fb2782660a9a128d5fdfc8df5ae73ad1ca57b
Charles Bergeron <chuck.bergeron@gmail.com>
2018-06-04 13:06:01 -0700

    * collapse 2 jest setups into the 1 in package.json

--- dapp/jest-setup.js
+++ dapp/jest-setup.js
@@ -1,6 +1,6 @@
 import Enzyme, { shallow, render, mount } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
-import Web3 from 'web3'
+// import Web3 from 'web3'
 
 // React 16 Enzyme adapter
 Enzyme.configure({ adapter: new Adapter() });
@@ -9,8 +9,8 @@ Enzyme.configure({ adapter: new Adapter() });
 global.shallow = shallow;
 global.render = render;
 global.mount = mount;
-global.Web3 = Web3
 
-const provider = new Web3.providers.HttpProvider("http://localhost:8545");
-global.window.web3 = new Web3(provider);
-global.window.web3.eth.defaultAccount = global.window.web3.eth.accounts[0]
+// global.Web3 = Web3
+// const provider = new Web3.providers.HttpProvider("http://localhost:8545");
+// global.window.web3 = new Web3(provider);
+// global.window.web3.eth.defaultAccount = global.window.web3.eth.accounts[0]
