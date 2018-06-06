22262d75aa3c43facb17df1004d008e8275ce3b7
Charles Bergeron <chuck.bergeron@gmail.com>
2018-06-06 13:46:11 -0700

    * adding in all current smart contract method translations

--- dapp/src/config/i18n.js
+++ dapp/src/config/i18n.js
@@ -11,9 +11,19 @@ i18next
       en: {
         translation: {
           transactions: {
+            acceptDiagnosis: 'Accept Diagnosis',
+            addDoctor: 'Adding Doctor',
+            approveAndCall: 'Sending Case Info',
+            authorizeChallengeDoctor: 'Authorizing Second Opinion',
+            authorizeDiagnosisDoctor: 'Authorizing Physician',
             blankState: 'Currently there are no pending transactions',
+            challengeDiagnosis: 'Getting Second Opinion',
+            diagnoseCase: 'Sending Diagnosis',
+            diagnoseChallengedCase: 'Sending Second Opinion',
+            getPublicKey: 'Getting Public Key',
             mint: 'Mint {{mintMedxCount}} MEDX',
-            approveAndCall: 'Submit New Case'
+            requestNextCase: 'Requesting Case',
+            setPublicKey: 'Setting Public Key'
           }
         }
       }
