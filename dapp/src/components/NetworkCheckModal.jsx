4ce935e067fb4c38a34edd8aade2992eb9be9dbb
Brendan Asselstine <mail.asselstine@gmail.com>
2018-06-12 14:48:29 -0700

    Network check modal is no longer done on the welcome page.  See #158278007

---
+++ dapp/src/components/NetworkCheckModal.jsx
@@ -0,0 +1,33 @@
+import React, { Component } from 'react'
+import { Modal } from 'react-bootstrap'
+import { connect } from 'react-redux'
+import get from 'lodash.get'
+import networkIdToName from '~/utils/network-id-to-name'
+
+function mapStateToProps(state, ownProps) {
+  const networkId = get(state, 'sagaGenesis.network.networkId')
+  return {
+    networkId
+  }
+}
+
+export const NetworkCheckModal = connect(mapStateToProps)(function({ networkId }) {
+  const requiredNetworkId = process.env.REACT_APP_REQUIRED_NETWORK_ID
+  if (requiredNetworkId &&
+      networkId !== parseInt(requiredNetworkId)) {
+    var requiredNetworkName = networkIdToName(parseInt(requiredNetworkId))
+    var showNetworkModal = true
+  }
+
+  return (
+    <Modal show={showNetworkModal}>
+      <Modal.Body>
+        <div className="row">
+          <div className="col-xs-12 text-center">
+            <h4>You must switch to the {requiredNetworkName} network</h4>
+          </div>
+        </div>
+      </Modal.Body>
+    </Modal>
+  )
+})
