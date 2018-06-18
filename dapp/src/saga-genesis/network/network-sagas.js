e494ba92f49ac4ea3165a7e7a619f02ea9884c6d
Brendan Asselstine <mail.asselstine@gmail.com>
2018-06-18 11:45:31 -0700

    Eliminated the annoying polling actions

--- dapp/src/saga-genesis/network/network-sagas.js
+++ dapp/src/saga-genesis/network/network-sagas.js
@@ -1,6 +1,7 @@
 import {
   put,
   getContext,
+  select,
   call,
   fork
 } from 'redux-saga/effects'
@@ -10,14 +11,17 @@ import {
 
 export function* refreshNetwork() {
   const web3 = yield getContext('web3')
+  const existingNetworkId = yield select((state) => state.sagaGenesis.network.networkId)
   let networkId = yield web3.eth.net.getId()
-  yield put({type: 'WEB3_NETWORK_ID', web3, networkId})
+  if (existingNetworkId !== networkId) {
+    yield put({type: 'WEB3_NETWORK_ID', web3, networkId})
+  }
 }
 
 export function* startNetworkPolling() {
   while (true) {
     yield call(refreshNetwork)
-    yield call(delay, 2000)
+    yield call(delay, 1000)
   }
 }
 
