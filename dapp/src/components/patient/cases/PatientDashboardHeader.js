7ab167101ff58e00c5494c710e0ad70cae3345db
Brendan Asselstine <mail.asselstine@gmail.com>
2018-06-20 13:24:34 -0700

    Patient cannot create case until their public key has been set
    see #158503646

--- dapp/src/components/patient/cases/PatientDashboardHeader.js
+++ dapp/src/components/patient/cases/PatientDashboardHeader.js
@@ -1,16 +1,58 @@
 import React, { Component } from 'react';
 import { withRouter } from 'react-router-dom';
+import { ErrorModal } from '~/components/ErrorModal'
 import FontAwesomeIcon from '@fortawesome/react-fontawesome';
 import faFileMedical from '@fortawesome/fontawesome-free-solid/faFileMedical';
+import { connect } from 'react-redux'
+import { contractByName } from '~/saga-genesis/state-finders'
+import { cacheCall, withSaga, cacheCallValue } from '~/saga-genesis'
 
-export const PatientDashboardHeader = class extends Component {
+function mapStateToProps(state, ownProps) {
+  const account = state.sagaGenesis.accounts[0]
+  const AccountManager = contractByName(state, 'AccountManager')
+  const publicKey = cacheCallValue(state, AccountManager, 'publicKeys', account)
+  return {
+    account,
+    publicKey
+  }
+}
+
+function* saga({ account, AccountManager }) {
+  if (!account || !AccountManager) { return }
+  yield cacheCall(AccountManager, 'publicKeys', account)
+}
+
+export const PatientDashboardHeader = connect(mapStateToProps)(withSaga(saga, { propTriggers: ['account', 'AccountManager'] })(class _PatientDashboardHeader extends Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      showPublicKeyModal: false
+    }
+  }
+
   navigateToNewCase = () => {
-    this.props.history.push('/patients/cases/new');
+    if (!this.props.publicKey) {
+      this.setState({showPublicKeyModal: true})
+    } else {
+      this.props.history.push('/patients/cases/new')
+    }
   }
 
   render() {
     return (
       <div className="header-card card">
+        <ErrorModal
+          show={this.state.showPublicKeyModal}
+          onHide={() => this.setState({showPublicKeyModal: false})}>
+          <div className='row'>
+            <div className='col-sm-12'>
+              <p>
+                Your account has not yet been set up.  You must wait until your account's public key has been
+              set in the blockchain.
+              </p>
+            </div>
+          </div>
+        </ErrorModal>
         <div className='card-body'>
           <div className='row'>
             <div className='col-md-8 col-sm-12'>
@@ -25,7 +67,7 @@ export const PatientDashboardHeader = class extends Component {
               <button
                 type="button"
                 className="btn btn-lg btn-success"
-                onClick={() => this.navigateToNewCase()}>
+                onClick={this.navigateToNewCase}>
                 <FontAwesomeIcon
                   icon={faFileMedical}
                   size='lg' />
@@ -38,6 +80,6 @@ export const PatientDashboardHeader = class extends Component {
       </div>
     );
   }
-}
+}))
 
 export const PatientDashboardHeaderContainer = withRouter(PatientDashboardHeader)
