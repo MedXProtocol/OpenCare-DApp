1fafc11bf07d992078b52a9b86fb8da5c0ce1f69
Charles Bergeron <chuck.bergeron@gmail.com>
2018-06-21 16:21:49 -0700

    * have the tx sent message pop up right away when the user submits their public key transaction

--- dapp/src/components/PublicKeyCheck.jsx
+++ dapp/src/components/PublicKeyCheck.jsx
@@ -64,7 +64,7 @@ export const PublicKeyCheck = connect(mapStateToProps)(
               isSubmitting: false
             })
           })
-          .onReceipt(() => {
+          .onTxHash(() => {
             toastr.success('Your public key transaction has been sent.')
           })
           .onConfirmed(() => {
