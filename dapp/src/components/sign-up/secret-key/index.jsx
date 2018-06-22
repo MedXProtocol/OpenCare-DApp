037c16fc326af2f4caccb13253fc783f5f88e843
Charles Bergeron <chuck.bergeron@gmail.com>
2018-06-22 15:34:37 -0700

    * add current ETH address to sign up page for email and print to pdf

--- dapp/src/components/sign-up/secret-key/index.jsx
+++ dapp/src/components/sign-up/secret-key/index.jsx
@@ -1,6 +1,8 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
+import { connect } from 'react-redux'
 import { Button, Modal } from 'react-bootstrap'
+import get from 'lodash.get'
 import FontAwesomeIcon from '@fortawesome/react-fontawesome';
 import faPrint from '@fortawesome/fontawesome-free-solid/faPrint';
 import faEnvelope from '@fortawesome/fontawesome-free-solid/faEnvelope';
@@ -8,6 +10,14 @@ import { BodyClass } from '~/components/BodyClass'
 import { formatSecretKey } from '~/services/format-secret-key'
 import * as routes from '~/config/routes'
 import { ScrollToTopOnMount } from '~/components/ScrollToTopOnMount'
+
+function mapStateToProps(state) {
+  let address = get(state, 'sagaGenesis.accounts[0]')
+
+  return {
+    address
+  }
+}
 
 export const SecretKey = class extends Component {
   constructor(props) {
@@ -22,7 +32,7 @@ export const SecretKey = class extends Component {
     e.preventDefault()
 
     let subject = 'Important! Hippocrates Secret Key -- KEEP THIS SAFE!'
-    let body    = `Dear Hippocrates user,%0A%0AYour secret key data is encrypted and stored safe using this secret key:%0A%0A${formatSecretKey(this.props.secretKey)}%0A%0AKeep this somewhere safe and secure, and use it to log in to Hippocrates from any other browser.%0A%0AThanks for using Hippocrates!%0A- The MedCredits Team`
+    let body    = `Dear Hippocrates user,%0A%0AYour secret key data is encrypted and stored safe using this secret key:%0A%0A${formatSecretKey(this.props.secretKey)}%0A%0AFor ETH address: ${this.props.address}%0A%0A%0AKeep this somewhere safe and secure, and use it to log in to Hippocrates from any other browser.%0A%0AThanks for using Hippocrates!%0A- The MedCredits Team`
 
     window.open(`mailto:?subject=${subject}&body=${body}`, '_blank', 'noopener');
   }
@@ -54,6 +64,9 @@ export const SecretKey = class extends Component {
                       {formatSecretKey(this.props.secretKey)}
                     </div>
                   </div>
+                  <p className="small text-center">
+                    <span className="eth-address text-gray">For eth address: {this.props.address}</span>
+                  </p>
                   <p>
                     You will need this key to access your account from new devices and browsers.
                     Without it you <em>will not</em> be able to view your previous cases. Save
@@ -137,3 +150,5 @@ export const SecretKey = class extends Component {
     )
   }
 }
+
+export const SecretKeyContainer = connect(mapStateToProps)(SecretKey)
