69e3e63a1c4afd51c4e6910a02e6a6c144eea99f
Charles Bergeron <chuck.bergeron@gmail.com>
2018-06-22 17:48:21 -0700

    * improve the loading indicator screen so it isn't jarring

--- dapp/src/components/Loading.js
+++ dapp/src/components/Loading.js
@@ -1,20 +1,27 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import { CSSTransition } from 'react-transition-group'
 import medCreditsLoadingImg from '~/assets/img/medcredits-logo--animated.svg';
 
 export const Loading = class extends Component {
   render() {
-    return this.props.loading ?
-      <div className="loading--overlay">
-        <div className="loading">
-          <img width="100" height="100" src={medCreditsLoadingImg} alt="MedCredits Loading Indicator" />
-          <p>
-            Working on your request ...
-            <br />
-            <span className="small">(You may need to check your MetaMask)</span>
-          </p>
+    return <CSSTransition
+        in={this.props.loading}
+        timeout={1000}
+        unmountOnExit
+        classNames="fade-slow"
+      >
+        <div className="loading--overlay">
+          <div className="loading">
+            <img width="100" height="100" src={medCreditsLoadingImg} alt="MedCredits Loading Indicator" />
+            <p>
+              Working on your request ...
+              <br />
+              <span className="small">(You may need to check MetaMask)</span>
+            </p>
+          </div>
         </div>
-      </div> : null;
+      </CSSTransition>
   }
 }
 
