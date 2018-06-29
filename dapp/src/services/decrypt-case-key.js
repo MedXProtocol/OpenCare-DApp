8ffbff6e2fe73c5191252e1d4fc05a388465f56a
Charles Bergeron <chuck.bergeron@gmail.com>
2018-06-28 23:06:39 -0700

    * prevent the decrypt caseKey error message from showing up by setting caseKey to undefined to start, and testing for null (unable to decrypt)

--- dapp/src/services/decrypt-case-key.js
+++ dapp/src/services/decrypt-case-key.js
@@ -1,19 +1,19 @@
 import { cacheCallValue } from '~/saga-genesis'
 
 export const decryptCaseKey = function (state, account, caseAddress) {
-  let caseKey
+  let caseKey = undefined // undefined caseKey means it's still loading / state is unknown!
 
   const encryptedCaseKey = cacheCallValue(state, caseAddress, 'encryptedCaseKey')
   const caseKeySalt = cacheCallValue(state, caseAddress, 'caseKeySalt')
 
   if (encryptedCaseKey && caseKeySalt) {
     caseKey = account.decrypt(encryptedCaseKey.substring(2), caseKeySalt.substring(2))
-  }
 
-  // Test if the caseKey is undefined or NaN (if we were able to decrypt it properly)
-  // This should be replaced with an HMAC test
-  if (isNaN(parseInt(caseKey, 16)) || parseInt(caseKey, 16) === undefined)
-    caseKey = null
+    // Test if the caseKey is undefined or NaN (if we were able to decrypt it properly)
+    // This should be replaced with an HMAC test
+    if (isNaN(parseInt(caseKey, 16)) || parseInt(caseKey, 16) === undefined)
+      caseKey = null
+  }
 
   return caseKey
 }
