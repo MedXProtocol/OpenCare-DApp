7e69c9c1df1bc5c5f6ecfce6041d02fb9d8a7f2e
Brendan Asselstine <mail.asselstine@gmail.com>
2018-07-04 14:02:19 -0700

    Removed some dead code

--- dapp/src/accounts/destroy-account.js
+++
@@ -1,8 +0,0 @@
-import { KEY_STORE } from '~/accounts/constants'
-import { setCookie } from '~/services/setCookie'
-
-export function destroyAccount(address) {
-  const keyStore = Cookie.getJSON(KEY_STORE) || {}
-  delete keyStore[address]
-  setCookie(KEY_STORE, keyStore)
-}
