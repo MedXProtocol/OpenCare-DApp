c4f573eae704630e9e07a69494700f890b708572
Brendan Asselstine <mail.asselstine@gmail.com>
2018-07-09 16:51:51 -0700

    Changed npm install to npm ci for automated builds

--- scripts/staging-build.sh
+++ scripts/staging-build.sh
@@ -3,4 +3,4 @@
 truffle compile && \
 npm run migrate -- --network ropsten && \
 ./lambda-build.sh && \
-cd dapp && npm install && npm run build && cd ..
+cd dapp && npm ci && npm run build && cd ..
