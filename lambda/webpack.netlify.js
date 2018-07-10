88484a5171f37255d99181c879f8e496fb016bbc
Charles Bergeron <chuck.bergeron@gmail.com>
2018-07-09 22:35:19 -0700

    * webpack did not like that

--- lambda/webpack.netlify.js
+++ lambda/webpack.netlify.js
@@ -10,7 +10,7 @@ module.exports = {
     ]
   },
   resolve: {
-    extensions: ['.js', '.jsx', '.node', '.json', ''],
+    extensions: ['.js', '.jsx', '.node', '.json'],
     modules: ["node_modules", "src"]
   },
   externals: [
