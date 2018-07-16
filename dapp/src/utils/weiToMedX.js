2381b600a590417d99b834e3f19a009bd9e3d86b
Brendan Asselstine <mail.asselstine@gmail.com>
2018-07-16 12:46:17 -0700

    Fixed weiToMedX

--- dapp/src/utils/weiToMedX.js
+++ dapp/src/utils/weiToMedX.js
@@ -3,5 +3,6 @@ import Web3 from 'web3'
 const web3 = new Web3()
 
 export function weiToMedX(amount) {
-  return web3.utils.fromWei(amount, 'ether')
+  if (!amount) { return 0 }
+  return web3.utils.fromWei(amount.toString(), 'ether')
 }
