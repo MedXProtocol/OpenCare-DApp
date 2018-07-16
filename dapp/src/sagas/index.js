6fd24f4eaa286afbd427e7763bd51b785c07efda
Charles Bergeron <chuck.bergeron@gmail.com>
2018-07-16 14:51:36 -0700

    * fix dupe saga

--- dapp/src/sagas/index.js
+++ dapp/src/sagas/index.js
@@ -24,7 +24,6 @@ export default function* () {
       signUpSaga(),
       nextAvailableDoctorSaga(),
       // heartbeatSaga(),
-      nextAvailableDoctorSaga(),
       pollExternalTransactionsSaga()
     ])
   })
