05d130f048c52e55504676ac7afc18eb3b05c25c
Charles Bergeron <chuck.bergeron@gmail.com>
2018-07-16 14:21:42 -0700

    * merge conflict issue

--- dapp/src/sagas/index.js
+++ dapp/src/sagas/index.js
@@ -22,7 +22,7 @@ export default function* () {
       signInSaga(),
       signOutSaga(),
       signUpSaga(),
-      nextAvailableDoctorSaga()
+      nextAvailableDoctorSaga(),
       // heartbeatSaga(),
       nextAvailableDoctorSaga(),
       pollExternalTransactionsSaga()
