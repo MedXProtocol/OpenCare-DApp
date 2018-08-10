f16880d2f3ce8c9c5f07969f97e1fe0433b7bda5
Brendan Asselstine <mail.asselstine@gmail.com>
2018-08-10 11:20:54 -0700

    Added better error handling to case migration

--- migrations/1533922334_update_cases.js
+++ migrations/1533922334_update_cases.js
@@ -2,6 +2,25 @@ let CaseManager = artifacts.require("./CaseManager.sol");
 let Registry = artifacts.require('./Registry.sol');
 let Case = artifacts.require('./Case.sol');
 
+async function migrateCase(cm, index) {
+  const caseAddress = await cm.caseList(index)
+  const case = await Case.at(caseAddress)
+  const diagnosingDoctor = await case.diagnosingDoctor()
+  const challengingDoctor = await case.challengingDoctor()
+  const status = await case.status()
+  if (status == '4' || status == '7' || status == '8') {
+    await cm.addClosedCase(diagnosingDoctor, caseAddress)
+    if (challengingDoctor) {
+      await cm.addClosedCase(challengingDoctor, caseAddress)
+    }
+  } else {
+    await cm.addOpenCase(diagnosingDoctor, caseAddress)
+    if (challengingDoctor) {
+      await cm.addOpenCase(challengingDoctor, caseAddress)
+    }
+  }
+}
+
 module.exports = function(deployer) {
   deployer.then(async () => {
     const registryInstance = await Registry.deployed()
@@ -10,21 +29,11 @@ module.exports = function(deployer) {
     const caseCount = await cm.getAllCaseListCount()
 
     for (var i = 1; i <= caseCount; i++) {
-      const caseAddress = await cm.caseList(i)
-      const case = await Case.at(caseAddress)
-      const diagnosingDoctor = await case.diagnosingDoctor()
-      const challengingDoctor = await case.challengingDoctor()
-      const status = await case.status()
-      if (status == '4' || status == '7' || status == '8') {
-        await cm.addClosedCase(diagnosingDoctor, caseAddress)
-        if (challengingDoctor) {
-          await cm.addClosedCase(challengingDoctor, caseAddress)
-        }
-      } else {
-        await cm.addOpenCase(diagnosingDoctor, caseAddress)
-        if (challengingDoctor) {
-          await cm.addOpenCase(challengingDoctor, caseAddress)
-        }
+      try {
+        await migrateCase(cm, i)
+        console.log('Migrated case ', i)
+      } catch (error) {
+        console.warn('Case already migrated: ', i)
       }
     }
   })
