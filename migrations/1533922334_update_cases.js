18d3cef6606168fa942590f1d7c14da23a611f9a
Charles Bergeron <chuck.bergeron@gmail.com>
2018-08-10 10:56:42 -0700

    * working on case migration

--- migrations/1533922334_update_cases.js
+++ migrations/1533922334_update_cases.js
@@ -1,3 +1,13 @@
+let CaseManager = artifacts.require("./CaseManager.sol");
+let Registry = artifacts.require('./Registry.sol');
+
 module.exports = function(deployer) {
-  // Use deployer to state migration tasks.
+  const registryInstance = Registry.deployed()
+
+  CaseManager.at(registryInstance.methods.lookup(web3.sha3('CaseManager'))).then(i => cm = i)
+  const caseIndices = cm.caseIndices.call()
+
+  for (var i = 0; i < caseIndices.length; i++){
+
+  }
 };
