ccd9f9390e5730d17982f786a01ecb873e6cafec
Brendan Asselstine <mail.asselstine@gmail.com>
2018-08-10 11:07:31 -0700

    Preliminary implementation of case migration

--- migrations/1533922334_update_cases.js
+++ migrations/1533922334_update_cases.js
@@ -1,13 +1,31 @@
 let CaseManager = artifacts.require("./CaseManager.sol");
 let Registry = artifacts.require('./Registry.sol');
+let Case = artifacts.require('./Case.sol');
 
 module.exports = function(deployer) {
-  const registryInstance = Registry.deployed()
+  deployer.then(async () => {
+    const registryInstance = await Registry.deployed()
+    const CaseManagerAddress = await registryInstance.lookup(web3.sha3('CaseManager'))
+    const cm = await CaseManager.at(CaseManagerAddress)
+    const caseCount = await cm.getAllCaseListCount()
 
-  CaseManager.at(registryInstance.methods.lookup(web3.sha3('CaseManager'))).then(i => cm = i)
-  const caseIndices = cm.caseIndices.call()
-
-  for (var i = 0; i < caseIndices.length; i++){
-
-  }
+    for (var i = 1; i <= caseCount; i++) {
+      const caseAddress = await cm.caseList(i)
+      const case = await Case.at(caseAddress)
+      const diagnosingDoctor = await case.diagnosingDoctor()
+      const challengingDoctor = await case.challengingDoctor()
+      const status = await case.status()
+      if (status == '4' || status == '7' || status == '8') {
+        await cm.addClosedCase(diagnosingDoctor, caseAddress)
+        if (challengingDoctor) {
+          await cm.addClosedCase(challengingDoctor, caseAddress)
+        }
+      } else {
+        await cm.addOpenCase(diagnosingDoctor, caseAddress)
+        if (challengingDoctor) {
+          await cm.addOpenCase(challengingDoctor, caseAddress)
+        }
+      }
+    }
+  })
 };
