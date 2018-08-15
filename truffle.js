531aef29d52c244ac442ef51f63e103e9ad7ee96
Brendan Asselstine <mail.asselstine@gmail.com>
2018-08-15 15:55:30 -0700

    Added gas limit to ganache and dropped gas limit in truffle.js

--- truffle.js
+++ truffle.js
@@ -9,19 +9,19 @@ module.exports = {
       host: "127.0.0.1",
       port: 8545,
       network_id: 1234,
-      gas: 4712394,
+      gas: 4700000,
       gasPrice: 60 * 1000000000
     },
     ropsten: {
       provider: () => new HDWalletProvider(process.env.HDWALLET_MNEMONIC, process.env.ROPSTEN_PROVIDER_URL),
       network_id: 3,
-      gas: 4712394,
+      gas: 4700000,
       gasPrice: 60 * 1000000000
     },
     rinkeby: {
       provider: () => new HDWalletProvider(process.env.HDWALLET_MNEMONIC, process.env.RINKEBY_PROVIDER_URL),
       network_id: 4,
-      gas: 4712394,
+      gas: 4700000,
       gasPrice: 10 * 1000000000
     }
   },
