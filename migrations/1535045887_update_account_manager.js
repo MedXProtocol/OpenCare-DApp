ea00d44a5688e6bdce597456a3ee09fa55692d10
Charles Bergeron <chuck.bergeron@gmail.com>
2018-08-23 13:43:47 -0700

    * scrap unnecessary migrations

--- migrations/1535045887_update_account_manager.js
+++
@@ -1,8 +0,0 @@
-var deployAndRegister = require('./support/deploy-and-register')
-
-let AccountManager = artifacts.require("./AccountManager.sol");
-let Registry = artifacts.require('./Registry.sol');
-
-module.exports = function(deployer) {
-  return deployAndRegister(deployer, AccountManager, Registry, 'AccountManagerTarget')
-};
