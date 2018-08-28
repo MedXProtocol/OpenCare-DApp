ccd4b0916b7e06159f187aed4a2f21720df95ab2
Brendan Asselstine <mail.asselstine@gmail.com>
2018-08-28 10:31:58 -0700

    Fixed test

--- dapp/src/utils/__tests__/displayWeiToUsd.test.js
+++ dapp/src/utils/__tests__/displayWeiToUsd.test.js
@@ -6,9 +6,9 @@ describe('displayWeiToUsd', () => {
     const web3 = new Web3()
 
     expect(displayWeiToUsd(web3.utils.toWei('1235213.1299999', 'ether')))
-      .toEqual('$1,235,213.13')
+      .toEqual('1,235,213.13')
 
     expect(displayWeiToUsd(web3.utils.toWei('1200', 'ether')))
-      .toEqual('$1,200.00')
+      .toEqual('1,200.00')
   })
 })
