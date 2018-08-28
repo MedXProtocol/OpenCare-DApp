e18aa4bc89c43788c22b2686a46a8b897f79eb10
Charles Bergeron <chuck.bergeron@gmail.com>
2018-08-28 09:52:12 -0600

    * open zeppelin moved a contract

--- contracts/MedXToken.sol
+++ contracts/MedXToken.sol
@@ -1,7 +1,7 @@
 pragma solidity ^0.4.23;
 
 import "./ITokenRecipient.sol";
-import "zeppelin-solidity/contracts/token/MintableToken.sol";
+import "zeppelin-solidity/contracts/token/ERC20/MintableToken.sol";
 
 contract MedXToken is MintableToken {
     string public constant name = "MedX Token";
