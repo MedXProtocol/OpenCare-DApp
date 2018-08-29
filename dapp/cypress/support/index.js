634fa1ca9c306d7f806f298baf5d4a581d7f725f
Charles Bergeron <chuck.bergeron@gmail.com>
2018-08-29 15:52:07 -0600

    * still can't get it to work

--- dapp/cypress/support/index.js
+++ dapp/cypress/support/index.js
@@ -19,3 +19,19 @@ import './commands'
 // Alternatively you can use CommonJS syntax:
 // require('./commands')
 
+
+// import Web3 from "web3"
+// import PrivateKeyProvider from "truffle-privatekey-provider"
+console.log('etf')
+Cypress.on("window:before:load", (win) => {
+  console.log('yoooooooooo', win)
+
+  // cy.log(process.env.LAMBDA_CONFIG_PRIVKEY)
+  // cy.log('7777776821345768t134i876t234g')
+  //
+  // const provider = new PrivateKeyProvider(
+  //   process.env.LAMBDA_CONFIG_PRIVKEY,
+  //   process.env.LAMBDA_CONFIG_PROVIDER_URL
+  // )
+  // win.web3 = new Web3(provider) // eslint-disable-line no-param-reassign
+});
