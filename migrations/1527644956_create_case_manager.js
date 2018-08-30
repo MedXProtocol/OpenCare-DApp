b18c0c9f5800ed091854fb2620a6cf206915a691
Brendan Asselstine <mail.asselstine@gmail.com>
2018-08-30 12:24:55 -0700

    Fixed original create case manaager migration

--- migrations/1527644956_create_case_manager.js
+++ migrations/1527644956_create_case_manager.js
@@ -7,8 +7,8 @@ module.exports = function(deployer, network, accounts) {
   deployer.then(async () => {
     let registryInstance = await Registry.deployed()
     return deployWithDelegate(artifacts, deployer, CaseManager).then((caseManager) => {
-      let caseFee = web3.toWei('0.056', 'ether')
-      return caseManager.initialize(caseFee, registryInstance.address)
+      let caseFeeUsd = web3.toWei('15', 'ether')
+      return caseManager.initialize(caseFeeUsd, registryInstance.address)
     })
   })
 }
