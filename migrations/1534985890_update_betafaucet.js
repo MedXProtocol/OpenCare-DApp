7e7e8b55a7e9f81de0cf13bf3995309c92881e4d
Charles Bergeron <chuck.bergeron@gmail.com>
2018-08-31 13:21:11 -0700

    * get rid of the update migrations as we don't need them

--- migrations/1534985890_update_betafaucet.js
+++
@@ -1,7 +0,0 @@
-const BetaFaucet = artifacts.require('BetaFaucet.sol')
-const Registry = artifacts.require('Registry.sol')
-const deployAndRegister = require('./support/deploy-and-register')
-
-module.exports = function(deployer) {
-  return deployAndRegister(deployer, BetaFaucet, Registry, 'BetaFaucetTarget')
-};
