5dd86bb32a4683ebb13137e5c7166a7d060a8e96
Brendan Asselstine <mail.asselstine@gmail.com>
2018-09-03 12:00:41 -0700

    Re-enabled webrtc star

--- dapp/src/services/newNode.js
+++ dapp/src/services/newNode.js
@@ -1,21 +1,21 @@
 export async function newNode(_options) {
-  // const WebRTCStar = await import(/* webpackChunkName: 'p2p' */ 'libp2p-webrtc-star')
-  const WSStar = await import(/* webpackChunkName: 'p2p' */ 'libp2p-websocket-star')
+  const WebRTCStar = await import(/* webpackChunkName: 'p2p' */ 'libp2p-webrtc-star')
+  // const WSStar = await import(/* webpackChunkName: 'p2p' */ 'libp2p-websocket-star')
   const Mplex = await import(/* webpackChunkName: 'p2p' */ 'libp2p-mplex')
   const SECIO = await import(/* webpackChunkName: 'p2p' */ 'libp2p-secio')
   const defaultsDeep = await import(/* webpackChunkName: 'p2p' */ '@nodeutils/defaults-deep')
   const libp2p = await import(/* webpackChunkName: 'p2p' */ 'libp2p')
   const WebSockets = await import(/* webpackChunkName: 'p2p' */ 'libp2p-websockets')
 
-  // const wrtcStar = new WebRTCStar({ id: _options.peerInfo.id })
-  const wsStar = new WSStar({ id: _options.peerInfo.id })
+  const wrtcStar = new WebRTCStar({ id: _options.peerInfo.id })
+  // const wsStar = new WSStar({ id: _options.peerInfo.id })
 
   const defaults = {
     modules: {
       transport: [
         new WebSockets(),
-        // wrtcStar,
-        wsStar
+        wrtcStar,
+        // wsStar
       ],
       streamMuxer: [
         Mplex
@@ -24,17 +24,17 @@ export async function newNode(_options) {
         SECIO
       ],
       peerDiscovery: [
-        // wrtcStar.discovery,
-        wsStar.discovery
+        wrtcStar.discovery,
+        // wsStar.discovery
       ]
     },
     config: {
       peerDiscovery: {
         webRTCStar: {
-          enabled: false
+          enabled: true
         },
         websocketStar: {
-          enabled: true
+          enabled: false
         }
       },
       EXPERIMENTAL: {
