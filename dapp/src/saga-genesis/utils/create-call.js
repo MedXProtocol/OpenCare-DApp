f211b7a4706e4574e7abe55388ab32db1e662acd
Brendan Asselstine <mail.asselstine@gmail.com>
2018-09-04 10:27:24 -0700

    Added more insight to failed transactions
    [see #160035952]

--- dapp/src/saga-genesis/utils/create-call.js
+++ dapp/src/saga-genesis/utils/create-call.js
@@ -1,7 +1,14 @@
 import hashCall from './hash-call'
 
 export function createCall(address, method, ...args) {
-  let call = { address, method, args }
+  let call = {
+    address,
+    method,
+    args,
+    toString: function () {
+      return `${address}: ${method}(${args.map(a => a.toString()).join(', ')})`
+    }
+  }
   call.hash = hashCall(address, method, ...args)
   return call
 }
