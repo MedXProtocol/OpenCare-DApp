eb3f68e495c09c58395198c03aaa689af0f0b1a1
Charles Bergeron <chuck.bergeron@gmail.com>
2018-09-05 10:56:32 -0700

    * move uglify to bottom of plugins load order

--- dapp/config/webpack.config.dev.js
+++ dapp/config/webpack.config.dev.js
@@ -14,6 +14,9 @@ const ModuleScopePlugin = require('react-dev-utils/ModuleScopePlugin');
 const getClientEnvironment = require('./env');
 const paths = require('./paths');
 
+const SWPrecacheWebpackPlugin = require('sw-precache-webpack-plugin');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+
 // Webpack uses `publicPath` to determine where the app is being served from.
 // In development, we always serve from the root. This makes config easier.
 const publicPath = '/';
@@ -217,6 +220,9 @@ module.exports = {
     ],
   },
   plugins: [
+    new UglifyJsPlugin({
+      sourceMap: true
+    }),
     new CopyWebpackPlugin([
       { from: './config/_redirects' }
     ]),
@@ -226,10 +232,10 @@ module.exports = {
     // In development, this will be an empty string.
     new InterpolateHtmlPlugin(env.raw),
     // Generates an `index.html` file with the <script> injected.
-    new HtmlWebpackPlugin({
-      inject: true,
-      template: paths.appHtml,
-    }),
+    // new HtmlWebpackPlugin({
+    //   inject: true,
+    //   template: paths.appHtml,
+    // }),
     // Add module names to factory functions so they appear in browser profiler.
     new webpack.NamedModulesPlugin(),
     // Makes some environment variables available to the JS code, for example:
@@ -252,7 +258,56 @@ module.exports = {
     // https://github.com/jmblog/how-to-optimize-momentjs-with-webpack
     // You can remove this if you don't use Moment.js:
     new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/),
-    // new BundleAnalyzerPlugin()
+    // new BundleAnalyzerPlugin(),
+
+        // Generate a service worker script that will precache, and keep up to date,
+        // the HTML & assets that are part of the Webpack build.
+        new SWPrecacheWebpackPlugin({
+          // By default, a cache-busting query parameter is appended to requests
+          // used to populate the caches, to ensure the responses are fresh.
+          // If a URL is already hashed by Webpack, then there is no concern
+          // about it being stale, and the cache-busting can be skipped.
+          dontCacheBustUrlsMatching: /\.\w{8}\./,
+          filename: 'service-worker.js',
+          logger(message) {
+            if (message.indexOf('Total precache size is') === 0) {
+              // This message occurs for every build and is a bit too noisy.
+              return;
+            }
+            if (message.indexOf('Skipping static resource') === 0) {
+              // This message obscures real errors so we ignore it.
+              // https://github.com/facebookincubator/create-react-app/issues/2612
+              return;
+            }
+            console.log(message);
+          },
+          minify: true,
+          // For unknown URLs, fallback to the index page
+          navigateFallback: publicUrl + '/index.html',
+          // Ignores URLs starting from /__ (useful for Firebase):
+          // https://github.com/facebookincubator/create-react-app/issues/2237#issuecomment-302693219
+          navigateFallbackWhitelist: [/^(?!\/__).*/],
+          // Don't precache sourcemaps (they're large) and build asset manifest:
+          staticFileGlobsIgnorePatterns: [/\.map$/, /asset-manifest\.json$/],
+        }),
+
+        // Generates an `index.html` file with the <script> injected.
+        new HtmlWebpackPlugin({
+          inject: true,
+          template: paths.appHtml,
+          minify: {
+            removeComments: true,
+            collapseWhitespace: true,
+            removeRedundantAttributes: true,
+            useShortDoctype: true,
+            removeEmptyAttributes: true,
+            removeStyleLinkTypeAttributes: true,
+            keepClosingSlash: true,
+            minifyJS: true,
+            minifyCSS: true,
+            minifyURLs: true,
+          },
+        }),
   ],
   // Some libraries import Node modules but don't use them in the browser.
   // Tell Webpack to provide empty mocks for them so importing them works.
