58b6e26f51a8db0723e99315a603771943f3e1ee
Charles Bergeron <chuck.bergeron@gmail.com>
2018-09-07 18:34:28 -0700

    * on sign out, stop heartbeats * on sign in, start heartbeats

--- dapp/src/sagas/heartbeat-saga.js
+++ dapp/src/sagas/heartbeat-saga.js
@@ -64,7 +64,7 @@ function* postHeartbeat(symKeyId, address) {
     })
   } catch (error) {
     console.error(error)
-    yield put({ type: 'WEB3_SHH_DISCONNECT', error })
+    yield put({ type: 'WEB3_SHH_ERROR', error })
   }
 }
 
