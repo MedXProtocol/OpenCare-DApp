871f8a09fd4a88a3c4462fab6c007b9d5d076f6f
Charles Bergeron <chuck.bergeron@gmail.com>
2018-09-08 23:28:19 -0700

    * another delegate case diagnosing doc contract try

---
+++ migrations/1536191000_update_case_diagnosing_doctor.js
@@ -0,0 +1,12 @@
+const deployWithDelegate = require('./support/deployWithDelegate')
+
+let CaseDiagnosingDoctor = artifacts.require("./CaseDiagnosingDoctor.sol");
+let Registry = artifacts.require('./Registry.sol');
+
+module.exports = function(deployer) {
+  deployer.then(async () => {
+    const registryInstance = await Registry.deployed()
+
+    return deployWithDelegate(artifacts, deployer, CaseDiagnosingDoctor)
+  })
+};
