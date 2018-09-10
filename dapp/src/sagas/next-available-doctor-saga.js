0d78e7a9b72610ac43e29e68115c9d6579085e24
Charles Bergeron <chuck.bergeron@gmail.com>
2018-09-10 12:39:07 -0700

    * online doc same one every time

--- dapp/src/sagas/next-available-doctor-saga.js
+++ dapp/src/sagas/next-available-doctor-saga.js
@@ -126,8 +126,9 @@ function* priorityDoctorOrOfflineDoctor() {
 }
 
 function* findNextAvailableOnlineDoctor () {
-  const onlineAddresses = Object.keys(yield select(state => state.heartbeat.users))
+  let onlineAddresses = Object.keys(yield select(state => state.heartbeat.users))
   let doctor = null
+  onlineAddresses = shuffle(onlineAddresses)
   for (var i = 0; i < onlineAddresses.length; i++) {
     doctor = yield fetchDoctorByAddress(onlineAddresses[i])
     if (doctor) { break }
