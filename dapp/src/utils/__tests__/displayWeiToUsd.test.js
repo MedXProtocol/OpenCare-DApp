a00817b5782f9bbe0c49a1c449c3cdf6b1807c87
Brendan Asselstine <mail.asselstine@gmail.com>
2018-09-13 13:16:48 -0700

    Added additional test cases to displayWeiToETher

--- dapp/src/utils/__tests__/displayWeiToUsd.test.js
+++ dapp/src/utils/__tests__/displayWeiToUsd.test.js
@@ -2,12 +2,10 @@ import Web3 from 'web3'
 import { displayWeiToUsd } from '../displayWeiToUsd'
 
 describe('displayWeiToUsd', () => {
-  test('Will comma and round decimals', () => {
+  test('Will comma and round to 2 decimals', () => {
     const web3 = new Web3()
-
     expect(displayWeiToUsd(web3.utils.toWei('1235213.1299999', 'ether')))
       .toEqual('1,235,213.13')
-
     expect(displayWeiToUsd(web3.utils.toWei('1200', 'ether')))
       .toEqual('1,200')
   })
--- dapp/src/utils/displayWeiToEther.js
+++ dapp/src/utils/displayWeiToEther.js
@@ -2,9 +2,10 @@ import BN from 'bn.js'
 import { weiToEther } from './weiToEther'
 
 export function displayWeiToEther(wei, decimalPlaces = 4) {
-  let etherInWei = new BN('1000000000000000000', 10)
-  let divisor = etherInWei.divRound(new BN(10).pow(new BN(decimalPlaces)))
-  let rounded = new BN(wei, 10).divRound(divisor).toString(10)
+  if (!wei) { return '0' }
+  const etherInWei = new BN('1000000000000000000', 10)
+  const divisor = etherInWei.divRound(new BN(10).pow(new BN(decimalPlaces)))
+  const rounded = new BN(wei, 10).divRound(divisor).toString(10)
   const fraction = rounded.slice(-decimalPlaces)
   let ethers = rounded.slice(0, -decimalPlaces)
   ethers = ethers.replace(/\d(?=(\d{3})+$)/g, '$&,')
