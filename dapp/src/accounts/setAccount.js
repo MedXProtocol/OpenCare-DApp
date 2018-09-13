0019adbbe948f6c5afc6038c9321ae292bc1ede7
Charles Bergeron <chuck.bergeron@gmail.com>
2018-09-13 14:10:48 -0700

    * remove this throw

--- dapp/src/accounts/setAccount.js
+++ dapp/src/accounts/setAccount.js
@@ -4,7 +4,5 @@ import { setAccountLocalStorage } from './setAccountLocalStorage'
 export function setAccount(networkId, address, accountObject) {
   if (storageAvailable('localStorage')) {
     setAccountLocalStorage(networkId, address, accountObject)
-  } else {
-    throw new Error('Unable to set account! Possibly no access to localStorage')
   }
 }
