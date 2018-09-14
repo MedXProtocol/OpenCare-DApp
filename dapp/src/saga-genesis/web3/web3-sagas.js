85fade14d8331480848f204c8ba3c36e19e655e4
Charles Bergeron <chuck.bergeron@gmail.com>
2018-09-14 15:34:13 -0700

    * force web3 beta 35 * try our own direct provider for sagas

--- dapp/src/saga-genesis/web3/web3-sagas.js
+++ dapp/src/saga-genesis/web3/web3-sagas.js
@@ -6,7 +6,17 @@ import Web3 from 'web3'
 
 function* web3Initialize() {
   if (window.web3) {
-    const web3 = new Web3(window.web3.currentProvider)
+    let web3
+    console.log(typeof process.env.REACT_APP_REQUIRED_NETWORK_ID)
+    console.log(process.env.REACT_APP_REQUIRED_NETWORK_ID)
+    if (process.env.REACT_APP_REQUIRED_NETWORK_ID === '4') {
+      web3 = new Web3(process.env.RINKEBY_PROVIDER_URL)
+      console.log('web3 set to ', process.env.RINKEBY_PROVIDER_URL)
+    } else {
+      web3 = new Web3(window.web3.currentProvider)
+      console.log('web3 set to ', window.web3.currentProvider)
+    }
+
     yield put({type: 'WEB3_INITIALIZED', web3})
   } else {
     console.error("window.web3 doesn't exist!")
