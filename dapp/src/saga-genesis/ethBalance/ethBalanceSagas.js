a09a1ba93195c955c8b94adc2e0b88fb7da73981
Charles Bergeron <chuck.bergeron@gmail.com>
2018-09-18 10:15:12 -0700

    * pull 1 custom http provider set for the given provider, and try setting only the http provider on the custom web3

--- dapp/src/saga-genesis/ethBalance/ethBalanceSagas.js
+++ dapp/src/saga-genesis/ethBalance/ethBalanceSagas.js
@@ -1,16 +1,18 @@
-import { put, select, call, fork } from 'redux-saga/effects'
+import { getContext, put, select, call, fork } from 'redux-saga/effects'
 import { delay } from 'redux-saga'
 import { bugsnagClient } from '~/bugsnagClient'
-import { customProviderWeb3 } from '~/utils/customProviderWeb3'
+// import { customProviderWeb3 } from '~/utils/customProviderWeb3'
 
 function* getEthBalance() {
-  const web3 = customProviderWeb3()
+  const web3 = yield getContext('web3')
+  // const web3 = customProviderWeb3()
   const address = yield select((state) => state.sagaGenesis.accounts[0])
   if (web3 === undefined || address === undefined) {
     return
   }
   const balance = yield call(web3.eth.getBalance, address)
   const oldBalance = yield select(state => state.sagaGenesis.ethBalance.balance)
+  console.log(balance, oldBalance)
   if (oldBalance !== balance) {
     yield put({type: 'ETH_BALANCE', balance})
   }
@@ -23,7 +25,7 @@ function* startEthBalancePolling() {
     } catch (e) {
       bugsnagClient.notify(e)
     }
-    yield call(delay, 2000)
+    yield call(delay, 10000)
   }
 }
 
--- dapp/src/utils/customProviderWeb3.js
+++ dapp/src/utils/customProviderWeb3.js
@@ -28,8 +28,10 @@ export const customProviderWeb3 = function() {
     throw new Error('*_PROVIDER_URL is undefined')
   }
 
-  // console.log('setting customWeb3 to ', customProvider)
-  customWeb3 = new Web3(customProvider)
+  console.log('setting customWeb3 to ', customProvider)
+  customWeb3 = new Web3(new Web3.providers.HttpProvider(customProvider))
+  console.log(customWeb3)
+  // customWeb3 = new Web3(customProvider)
 
   return customWeb3
 }
