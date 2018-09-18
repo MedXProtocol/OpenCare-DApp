e8444d7b89c7b2c230378e5a15effbe7c36e4e66
Charles Bergeron <chuck.bergeron@gmail.com>
2018-09-18 16:40:29 -0700

    * clean this up a bit

--- dapp/src/finders/transactionFinders.js
+++ dapp/src/finders/transactionFinders.js
@@ -5,14 +5,10 @@ export const transactionFinders = {
     const txs = Object.values(get(state, 'sagaGenesis.transactions'))
 
     return txs.filter((tx) => {
-      if (tx.call) {
-        return (
-             tx.call.method === 'createAndAssignCase'
-          || tx.call.method === 'createAndAssignCaseWithPublicKey'
-        )
-      } else {
-        return false
-      }
+      return (tx.call && (
+           tx.call.method === 'createAndAssignCase'
+        || tx.call.method === 'createAndAssignCaseWithPublicKey'
+      ))
     })
   }
 }
