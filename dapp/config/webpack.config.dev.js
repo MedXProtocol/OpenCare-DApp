0f38dd455ec864eeeee3754b8ec96d7d4947bdd0
Brendan Asselstine <mail.asselstine@gmail.com>
2018-09-19 13:06:25 -0700

    Tweaked the dev webpack config to enable source maps

--- dapp/config/webpack.config.dev.js
+++ dapp/config/webpack.config.dev.js
@@ -31,7 +31,7 @@ module.exports = {
   // You may want 'eval' instead if you prefer to see the compiled output in DevTools.
   // See the discussion in https://github.com/facebookincubator/create-react-app/issues/343.
   // devtool: 'cheap-module-source-map',
-  devtool: '#eval-source-map',
+  devtool: '#source-map',
   // devtool: 'eval',
   // These are the "entry points" to our application.
   // This means they will be the "root" imports that are included in JS bundle.
