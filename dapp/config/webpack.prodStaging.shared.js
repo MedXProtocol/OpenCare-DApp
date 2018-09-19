7c5c19af3108f3d969f9e27a8daa068c5533ae96
Brendan Asselstine <mail.asselstine@gmail.com>
2018-09-19 12:46:10 -0700

    Added chunk to webpack

--- dapp/config/webpack.prodStaging.shared.js
+++ dapp/config/webpack.prodStaging.shared.js
@@ -214,6 +214,7 @@ module.exports = {
   plugins: [
     new webpack.optimize.CommonsChunkPlugin({
       name: 'commons',
+      filename: 'commons.[hash].js',
       minChunks: Infinity
     }),
 
