e5c8e960d963fa67260dc93f1f267a6c4f7ad4c9
Brendan Asselstine <mail.asselstine@gmail.com>
2018-09-26 15:38:40 -0700

    Improved contract test script
    - Can pass in the test to run
    - If test fails, docker instance is cleaned up in background

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,5 +1,6 @@
-docker run --rm -it -d --name ganache-cli -p 8145:8545 trufflesuite/ganache-cli:latest -e 10000000000 -l 4700038 -h 0.0.0.0 && \
-  truffle compile --network=test && \
-  truffle migrate --network=test && \
-  truffle test --network=test && \
-  docker stop ganache-cli
+docker run --rm -it -d --name ganache-cli -p 8145:8545 trufflesuite/ganache-cli:latest -e 10000000000 -l 4700038 -h 0.0.0.0; \
+truffle compile --network=test && truffle test --network=test $1; \
+EXIT_STATUS=$?; \
+echo 'Shutting down ganache-cli...'; \
+docker stop ganache-cli && echo 'Shut down ganache-cli' & \
+exit $EXIT_STATUS
