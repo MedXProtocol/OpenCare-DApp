ce80cd1214cc8e157be4a1c8816a8e990ec1d592
Brendan Asselstine <mail.asselstine@gmail.com>
2018-10-02 11:50:36 -0700

    Do not deploy BetaFaucet on mainnet, and disable feature
    [see #160898609]

--- dapp/src/sagas/add-registry-contracts-saga.js
+++ dapp/src/sagas/add-registry-contracts-saga.js
@@ -7,12 +7,17 @@ import {
   addContract,
   web3Call
 } from '~/saga-genesis'
+const debug = require('debug')('addRegistryContractsSaga.js')
 
 function* lookupAndAddContract(web3, name) {
   const Registry = yield select(contractByName, 'Registry')
   const address = yield web3Call(Registry, 'lookup', web3.utils.sha3(name))
-  // console.log('called by add-registry-contracts-saga.js')
-  yield addContract({address, name, contractKey: name})
+  if (address) {
+    yield addContract({address, name, contractKey: name})
+    debug(`Added ${name} at ${address}`)
+  } else {
+    debug(`Skipping ${name} which is unregistered`)
+  }
 }
 
 export default function* ({ web3 }) {
