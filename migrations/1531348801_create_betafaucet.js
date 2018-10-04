4c8f15ebe00cadc5f16b07c52a030ba8a4813b88
Brendan Asselstine <mail.asselstine@gmail.com>
2018-10-04 13:42:37 -0700

    Fixed faulty migration

--- migrations/1531348801_create_betafaucet.js
+++ migrations/1531348801_create_betafaucet.js
@@ -9,7 +9,7 @@ module.exports = function(deployer, networkName) {
   deployer.then(async () => {
     const medXTokenInstance = await MedXToken.deployed()
 
-    if (deployer.network_id !== '1') { // if not mainnet
+    if (deployer.network_id !== 1) { // if not mainnet
       return deployTargetAndDelegate(artifacts, deployer, BetaFaucet, networkName).then(betaFaucetDelegateInstance => {
         return betaFaucetDelegateInstance.updateMedXTokenAddress(medXTokenInstance.address)
       })
