0be55956f50cc41236eeb8b8403de2edcb6a0d4f
Charles Bergeron <chuck.bergeron@gmail.com>
2018-10-12 12:18:07 -0700

    * fix an issue with yield/generator function in new saga

--- dapp/src/sagas/caughtErrorsSaga.js
+++ dapp/src/sagas/caughtErrorsSaga.js
@@ -2,7 +2,7 @@ import { takeEvery } from 'redux-saga/effects'
 import { bugsnagClient } from '~/bugsnagClient'
 
 export function* caughtErrorsSaga() {
-  yield takeEvery('SAGA_GENESIS_CAUGHT_ERROR', function* ({ error }) {
+  yield takeEvery('SAGA_GENESIS_CAUGHT_ERROR', function({ error }) {
     bugsnagClient.notify(error)
   })
 }
